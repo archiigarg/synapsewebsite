78732e33420fae06b6901743539b6eaa4de0a1e1
tanishka3001 <taani3001@gmail.com>
2025-01-21 23:16:30 +0530

    responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,11 @@ import heartImage from "./Vector.png";
 const Footer = () => {
   return (
     <footer className="bg-[#FFFF00] text-black p-8" style={{ fontFamily: 'Space Mono, monospace, Jost, sans-serif' }}>
-      <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center mt-8 border-t-2 border-black">
+      <div className="max-w-7xl mx-auto flex flex-row justify-center md:justify-between items-center mt-8 border-t-2 border-black">
         {/* Left Section: Content */}
-        <div className="flex flex-col md:border-l-2 border-black md:flex-row md:divide-x-2 md:divide-black space-y-8 mt-8 md:space-y-0 md:space-x-16">
+        <div className="flex flex-row md:border-l-2 border-black md:flex-row md:divide-x-2 md:divide-black space-y-8 mt-8 md:space-y-0 md:space-x-16">
           {/* Synapse Section */}
-          <div className="px-4">
+          <div className="px-4 hidden md:block">
             <h1 className="text-xs mb-4">SYNAPSE</h1>
             <ul className="space-y-2">
               <li>HOME</li>
@@ -21,8 +21,9 @@ const Footer = () => {
           </div>
 
           {/* ISTE Section */}
-          <div className="px-4">
-            <h1 className="text-xs mb-4">ISTE</h1>
+          <div className="px-4 text-center md:text-left">
+            <h1 className="text-xs mb-4 hidden sm:block">ISTE</h1>
+            <img src="/iste_logo.png" alt="iste-logo" className="w-10 h-10 mb-4 mx-auto sm:hidden" />
             <ul className="space-y-2">
               <li>WEBSITE</li>
               <li>INSTAGRAM</li>
@@ -33,8 +34,9 @@ const Footer = () => {
           </div>
 
           {/* IEEE Section */}
-          <div className="px-4">
-            <h1 className="text-xs mb-4">IEEE</h1>
+          <div className="px-4 text-center md:text-left" >
+            <h1 className="text-xs mb-4 hidden sm:block">IEEE</h1>
+            <img src="/ieee_logo.png" alt="ieee-logo" className="w-10 h-10 mb-4 mx-auto sm:hidden" />
             <ul className="space-y-2">
               <li>WEBSITE</li>
               <li>INSTAGRAM</li>
@@ -44,9 +46,10 @@ const Footer = () => {
             </ul>
           </div>
         </div>
+        
 
         {/* Right Section: Picture */}
-        <div className="mt-8 md:mt-0">
+        <div className="mt-8 md:mt-0 hidden md:block">
           <img
             src={SynapseLogo}
             alt="Synapse Logo"
@@ -55,7 +58,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="mt-8 pt-4 flex justify-start items-center space-x-2">
+      <div className="mt-8 pt-4 flex justify-center md:justify-start items-center space-x-2">
         <p className="flex items-center text-xs" style={{ fontFamily: 'Jost, sans-serif' }}>
           MADE WITH
           <img src={heartImage} alt="Heart" className="ml-1 mr-1 w-3 h-3" /> {/* Use the image here */}
